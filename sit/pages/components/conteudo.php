ece05978419295aa375bba3b69560d791d843279
Gabriel 2000 <166823901+gbr12122000@users.noreply.github.com>
2024-09-24 10:22:51 -0300

    Add files via upload
    php

---
+++ sit/pages/components/conteudo.php
@@ -0,0 +1,4 @@
+<div class="noticia-container">
+    <img src="https://media.istockphoto.com/id/513392620/pt/foto/grande-c%C3%A3o.jpg?s=612x612&w=0&k=20&c=ZKby_0nYtfIVC846s8BjA4msPiKoskh81vNJWu0a-ME=" class="noticia-imagem">
+    <a href="" class="noticia-titulo">Título da Notícia</a>
+</div>
---
+++ sit/pages/components/rodape.php
@@ -0,0 +1,19 @@
+<footer>
+    <div class="footer-conteudo">
+        <p>&copy; Gabriel</p>
+        <ul class="redes-sociais">
+            <li><a href="" target="_blank">Facebook</a></li>
+            <li><a href="" target="_blank">Twitter</a></li>
+            <li><a href="" target="_blank">Instagram</a></li>
+            <li><a href="" target="_blank">LinkedIn</a></li>
+        </ul>
+    </div>
+    <div class="footer-links">
+        <ul>
+            <li><a href="/sobre">Sobre Nós</a></li>
+            <li><a href="/contato">Contato</a></li>
+            <li><a href="/politica-de-privacidade">Política de Privacidade</a></li>
+            <li><a href="/termos-de-uso">Termos de Uso</a></li>
+        </ul>
+    </div>
+</footer>
